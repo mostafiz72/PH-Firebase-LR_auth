461c62e3584c531741b8d44431fa469b6c360626
Hera <mostafizhera@gmail.com>
2024-11-11 10:09:42 +0600

    adde the login and register authication project all setup

---
+++ Firebase/Firebase.inti.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBBpsUkIB4sKjlbligtLICa2xjqZP761XE",
+  authDomain: "login-register-auth-13935.firebaseapp.com",
+  projectId: "login-register-auth-13935",
+  storageBucket: "login-register-auth-13935.firebasestorage.app",
+  messagingSenderId: "329407638722",
+  appId: "1:329407638722:web:6e0ea84e2621fb87231f5b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
